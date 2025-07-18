48140c42b7d58ddcd0b0010c1efe4ed5c61c33d6
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2025-07-18 16:52:29 +0500

    Integrate Google Tag Manager and Clarity scripts; update cache URL logic (#57)

--- web/app/(public)/layout.tsx
+++ web/app/(public)/layout.tsx
@@ -1,3 +1,5 @@
+import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 import React from "react";
 
 export default function Layout({
@@ -5,5 +7,34 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <React.Fragment>{children}</React.Fragment>;
+  return (
+    <React.Fragment>
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MM8T8PXG"
+          height="0"
+          width="0"
+          style={{
+            display: "none",
+            visibility: "hidden",
+          }}
+        ></iframe>
+      </noscript>
+      {/* End Google Tag Manager (noscript) */}
+
+      {children}
+
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+            (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "sgt3ho599f");
+          `}
+      </Script>
+      <GoogleTagManager gtmId="GTM-MM8T8PXG" />
+    </React.Fragment>
+  );
 }
